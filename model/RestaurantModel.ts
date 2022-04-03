a8b0b370e34c7097ee823ccec92ef3fbbaf11998
Nicolas Debras <nicodebras17@gmail.com>
2022-04-03 20:56:11 +0200

    add model restaurant

---
+++ model/RestaurantModel.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose"
+import { UserProsp } from "./user_model";
+
+const RestaurantSchema = new Schema({
+    name : {
+        type: Schema.Types.String,
+        required: true
+    },
+    Cordlat : {
+        type: Schema.Types.Number,
+        Required: true,
+    },
+    Cordlong : {
+        type: Schema.Types.Number,
+        Required: true,
+    },
+    user : [{
+        type : Schema.Types.ObjectId
+    }] 
+
+}, {
+    timestamps: true,
+    versionKey: false,
+    collection : "restaurant"
+}
+);
+
+export interface RestaurantProsp {
+    name : string;
+    Cordlat : number;
+    Cordlong : number;
+    user : UserProsp[];
+    id :string;
+}
+
+export type RestaurantDocument = RestaurantProsp & Document;
+
+export const RestaurantModel = mongoose.model("restaurant", RestaurantSchema);
